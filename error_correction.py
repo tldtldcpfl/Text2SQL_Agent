7b80c76ea4023e50737c8974f6bb3095783a2a28
chaerin.jo <tldtldcpfl@gmail.com>
2026-04-29 17:29:43 +0900

    feat: add self-correction

---
+++ error_correction.py
@@ -0,0 +1,17 @@
+# self-correction loop (실행 기반 자동 보정) 
+from sqlalchemy import (
+    create_engine,
+    MetaData,
+    Table,
+    Column,
+    String,
+    Integer,
+    Float,
+    insert,
+    inspect,
+    text,
+)
+
+# sqlalchemy:
+# - 파이썬 데이터베이스 ORM(Object Realtaional Mapping) 라이브러리
+# - DB에 대한 CRUD 를 조작 
